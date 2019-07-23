3cc32c40bf82ab1f26edd064ab8cd0e3f85ea038
Milena <milenamolska@gmail.com>
2019-07-23 11:23:31 +0200

    Added cnn training and testing + visualisation
    train_visualisation - includes plot_model_loss and plot_model_accuracy

--- cnn_from_article/model.py
+++ cnn_from_article/model.py
@@ -2,6 +2,8 @@
 import os
 import random
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import numpy as np
+import pandas as pd
 
 dataset_input = []
 dataset_output = []
@@ -19,11 +21,14 @@ for i in images:
 	output = direction*[0] + [1] + (5-direction-1)*[0]
 	dataset_output.append(output)
 # input prepare
-	img = load_img(folder +'/' + i)
+	img = load_img(folder +'/' + i, color_mode="grayscale")
 	img.thumbnail((img_width, img_height))
-	img = img_to_array(img)a
+	img = img_to_array(img)
 	img = img / 255.0;
 	dataset_input.append(img)	
+
+dataset_input = np.array(dataset_input, dtype = "float")
+dataset_output = np.array(dataset_output)
 
 # Prepare dataset to learn: train-70%, val-15%, test-15%
 # split data to training and validation set
@@ -41,7 +46,6 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Flatten, Dense
 
 # CNN model
-
 model = Sequential([
 	Conv2D(32, (5,5), padding = 'same', input_shape = (img_height, img_width, 1), activation = 'relu'),
 	MaxPooling2D(pool_size = (2,2), strides = 2),
@@ -55,7 +59,33 @@ model = Sequential([
 	Dense(5, activation='linear')
 ])
 
-# TODO: learn cnn
+# training model
 model.compile(loss="mean_squared_error", optimizer='adam', metrics=["accuracy"])
-#hist = model.fit(x_train, y_train, batch_size=30, epochs=80, validation_data = (x_val, y_val))
 
+hist = model.fit(x_train, y_train, batch_size=30, epochs=10, validation_data = (x_val, y_val))
+model.summary()
+
+loss = model.evaluate(x_test, y_test)[0]
+accuracy = model.evaluate(x_test, y_test)[1]
+print("Test loss: ", loss)
+print("Test accuracy: ", accuracy)
+
+# saving model
+print('Do you want to save this model? y/n')
+answer = input()
+if answer == 'y':
+	print('Input filename:')
+	filename = input()
+	model.save(filename+'.model')
+	model.save(filename+'.h5')
+	model_json = model.to_json()
+	with open(filename+'.json', 'w') as json_file:
+		json_file.write(model_json)
+	hist_df = pd.DataFrame(hist.history) 
+	with open(filename+'_logg.csv', 'w') as f:
+    		hist_df.to_csv(f)
+
+# visualisation
+import train_visualisation
+train_visualisation.plot_model_loss(hist, answer, filename)
+train_visualisation.plot_model_accuracy(hist, answer, filename)
---
+++ cnn_from_article/train_visualisation.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def plot_model_loss(hist, answer, filename):
+	plt.plot(hist.history['loss'])
+	plt.plot(hist.history['val_loss'])
+	plt.title('Model loss')
+	plt.ylabel('Loss')
+	plt.xlabel('Epoch')
+	plt.legend(['Train', 'Val'], loc='upper right')
+	if answer == 'y':
+		plt.savefig(filename+'_loss_graph.png',dpi=400)
+	plt.show()
+
+def plot_model_accuracy(hist, answer, filename):
+	plt.plot(hist.history['acc'])
+	plt.plot(hist.history['val_acc'])
+	plt.title('Model accuracy')
+	plt.ylabel('Accuracy')
+	plt.xlabel('Epoch')
+	plt.legend(['Train', 'Val'], loc='lower right')
+	if answer == 'y':
+		plt.savefig(filename+'_acc_graph.png',dpi=400)
+	plt.show()
