67c4d9481005cfd267b52d2bff630ce111db5c4e
Milena <milenamolska@gmail.com>
2019-09-10 22:19:17 +0200

    Made code cleaner and prettier
    * Added universal module to save model 
    * Rewrite model.py script
    * Delete dataset but linked it in README.md

--- cnn_from_article/model.py
+++ cnn_from_article/model.py
@@ -1,105 +1,130 @@
 import os
+import re
 import random
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-import numpy as np
-import pandas as pd
 import datetime
-from model_description_to_txt import model_description_to_txt
 
-dataset_input = []
-dataset_output = []
-images = []
-folder = "./dataset"
-IMG_WIDTH = 160
-IMG_HEIGHT = 120
+import cv2
+import numpy as np
 
-# Load dataset
-images = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
-random.shuffle(images)
-
-for i in images:
-# Output prepare
-	direction = int(i[1:i.find('_')])
-	output = direction*[0] + [1] + (5-direction-1)*[0]
-	dataset_output.append(output)
-# Input prepare
-	img = load_img(folder +'/' + i, color_mode="grayscale")
-	img.thumbnail((IMG_WIDTH, IMG_HEIGHT))
-	img = img_to_array(img)
-	img = img / 255.0;
-	dataset_input.append(img)	
-
-dataset_input = np.array(dataset_input, dtype = "float")
-dataset_output = np.array(dataset_output)
-
-# Prepare dataset to learn: train-70%, val-15%, test-15%
-from sklearn.model_selection import train_test_split
-# Split data to training and validation set
-x_train, x_val_test, y_train, y_val_test = train_test_split(dataset_input, dataset_output, test_size = 0.3)
-# Split data to test and validation set
-x_val, x_test, y_val, y_test = train_test_split(x_val_test, y_val_test, test_size = 0.5)
-
-# tf & keras
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.callbacks import TensorBoard
-from keras.regularizers import l2, l1_l2
-from predictions_plot import plot_example_predictions
+from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Flatten
+from keras.layers import Dense, Dropout
+from keras.utils import plot_model
 
-# CNN model
-model = Sequential([
-	Conv2D(32, (5,5), padding = 'same', input_shape = (IMG_HEIGHT, IMG_WIDTH, 1), activation = 'relu'),
-	MaxPooling2D(pool_size = (2,2), strides = 2),
-	Conv2D(32, (5,5), padding = 'same', activation = 'relu'),
-	AveragePooling2D(pool_size = (2,2), strides = 2),
-	Conv2D(64, (5,5), padding = 'same', activation = 'relu'),
-	AveragePooling2D(pool_size = (2,2), strides = 2),
-	Flatten(),
-	Dense(256, activation = 'relu'),
-	Dropout(0.5),
-	Dense(128, activation = 'relu'),
-	Dense(5, activation = 'sigmoid')
-])
+from sklearn.model_selection import train_test_split
+from nn_register import save_nn_model, plot_model_loss, plot_model_accuracy
+from nn_register import model_info_to_txt, plot_predictions
 
-# Training 
-BATCH_SIZE = 16;
-EPOCHS = 200;
+# Global variables
+data_folder = './dataset/'
+img_width = 160
+img_height = 120
+filename_pattern = r'(?P<control>(a)\d)_\d\d?\d?.png'
+output_pattern = {
+    'a0': [1, 0, 0, 0, 0], # Full right
+    'a1': [0, 1, 0, 0, 0], # Half right
+    'a2': [0, 0, 1, 0, 0], # Straightforward
+    'a3': [0, 0, 0, 1, 0], # Half left
+    'a4': [0, 0, 0, 0, 1]  # Full left
+}
+output_labels = [
+    'Full right', 
+    'Half right', 
+    'Straightforward', 
+    'Half letf', 
+    'Full left'
+]
+batch_size = 20
+epochs = 200
+es_patience = 30
 
-model.compile(loss="mean_squared_error", optimizer='adam', metrics=["accuracy"])
-es = tf.keras.callbacks.EarlyStopping(monitor='val_acc', mode='max', patience = 30)
+def process_output_control(file_names):
+    regex = re.compile(filename_pattern)
+    control = [regex.search(f).group('control') for f in file_names]
+    output_data = [output_pattern.get(c) for c in control]
+    return np.array(output_data)
 
-hist = model.fit(x_train, y_train, batch_size = BATCH_SIZE, epochs = EPOCHS, validation_data = (x_val, y_val), callbacks = [es])
-model.summary()
+def process_depth_images(file_names):
+    data = [cv2.resize(cv2.imread(data_folder + f, \
+            cv2.IMREAD_GRAYSCALE), (img_width, img_height)).flatten() \
+            for f in file_names]
+    return np.array(data, dtype = 'float') / 255.0
 
-# Testing
-test_loss = model.evaluate(x_test, y_test)[0]
-test_accuracy = model.evaluate(x_test, y_test)[1]
-print("Test loss: ", test_loss)
-print("Test accuracy: ", test_accuracy)
-predictions = model.predict(x_test)
+def define_model():
+    model = Sequential([
+        Conv2D(32, (5,5), padding = 'same', input_shape = (img_height,\
+                 img_width, 1), activation = 'relu'),
+        MaxPooling2D(pool_size = (2,2), strides = 2),
+        Conv2D(32, (5,5), padding = 'same', activation = 'relu'),
+        AveragePooling2D(pool_size = (2,2), strides = 2),
+        Conv2D(64, (5,5), padding = 'same', activation = 'relu'),
+        AveragePooling2D(pool_size = (2,2), strides = 2),
+    	Flatten(),
+	    Dense(256, activation = 'relu'),
+        Dropout(0.25),
+        Dense(128, activation = 'relu'),
+	    Dense(5, activation = 'sigmoid')
+    ])
+    return model
 
-# Save model
-print('Do you want to save this model? y/n')
-answer = input()
-filename = ''
-if answer == 'y':
-	print('Input filename:')
-	filename = input()
-	os.mkdir("training/" + filename)
-	plot_example_predictions(filename, x_test, y_test, predictions)
-	model_description_to_txt(filename, model, BATCH_SIZE, EPOCHS, test_loss, test_accuracy)
-	model.save('training/' + filename + '/' + filename + '_model.model')
-	model.save('training/' + filename + '/' + filename + '_model.h5')
-	model_json = model.to_json()
-	with open('training/' + filename + '/' + filename + '_model.json', 'w') as json_file:
-		json_file.write(model_json)
-	hist_df = pd.DataFrame(hist.history) 
-	with open('training/' + filename + '/' + filename + '_logg.csv', 'w') as f:
-    		hist_df.to_csv(f)
 
-# Training visualisation
-import train_visualisation
-train_visualisation.plot_model_loss(hist, answer, filename)
-train_visualisation.plot_model_accuracy(hist, answer, filename)
+def main():
+    # Load dataset and make random sequence
+    images = [f for f in os.listdir(data_folder) \
+                if os.path.isfile(os.path.join(data_folder, f))]
+    random.shuffle(images)
+
+    # Process input and output data
+    output_data = process_output_control(images)
+    input_data = process_depth_images(images)
+
+    # Split data to training, validation and testing sets (70%, 15%, 15%)
+    x_train, x_val_test, y_train, y_val_test = train_test_split(\
+                input_data, output_data, test_size = 0.3)
+    x_val, x_test, y_val, y_test = train_test_split(\
+                x_val_test, y_val_test, test_size = 0.5)
+
+    # Reshape images to CNN input
+    x_train = x_train.reshape(len(x_train), img_height, img_width, 1)
+    x_test = x_test.reshape(len(x_test), img_height, img_width, 1)
+    x_val = x_val.reshape(len(x_val), img_height, img_width, 1)
+
+    # Define and compile CNN model
+    model = define_model()
+    model.compile(loss="mean_squared_error", optimizer='adam', \
+            metrics=["accuracy"])
+    es = tf.keras.callbacks.EarlyStopping(monitor = 'val_acc', \
+            mode = 'max', patience = es_patience)
+
+    # Learning network
+    hist = model.fit(x_train, y_train, batch_size = batch_size, \
+            epochs = epochs, validation_data = (x_val, y_val),  \
+            callbacks = [es])
+    model.summary()
+
+    # Test data predictions
+    test_loss = model.evaluate(x_test, y_test)[0]
+    test_accuracy = model.evaluate(x_test, y_test)[1]
+    print("Test loss: ", test_loss)
+    print("Test accuracy: ", test_accuracy)
+    predictions = model.predict(x_test)
+
+    # Saving model
+    print('Do you want to save this model? y/n')
+    answer = input()
+    if answer == 'y':
+        print('Input network name:')
+        filename = input()
+        if not os.path.exists('./training'):
+            os.mkdir('./training')
+        save_nn_model(filename, model, hist)
+        model_info_to_txt(filename, model, test_loss, test_accuracy)
+        plot_predictions(filename, x_test, y_test, predictions, \
+            output_labels)
+        plot_model_loss(filename, hist)
+        plot_model_accuracy(filename, hist)
+
+
+if __name__ == '__main__':
+    main()
